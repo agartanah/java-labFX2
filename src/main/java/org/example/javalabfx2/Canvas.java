03b07e1f21f0009b0913c42764e6a861dba887a1
agartanah <vyatin2017@mail.ru>
2024-10-31 18:28:02 +0700

    init

---
+++ src/main/java/org/example/javalabfx2/Canvas.java
@@ -0,0 +1,6 @@
+package org.example.javalabfx2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Canvas { }
---
+++ src/main/java/org/example/javalabfx2/Main.java
@@ -0,0 +1,13 @@
+package org.example.javalabfx2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException { }
+}
